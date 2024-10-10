f47be89a178e6c3d1d235e707d293312dfa980f0
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-10 11:03:21 +0200

    Manipulator class toegevoegd

---
+++ src/Manipulator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tigress;
+
+/**
+ * Class Text Manipulator (PHP version 8.3)
+ *
+ * @author Rudy Mas <rudy.mas@rudymas.be>
+ * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
+ * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * @version 1.0.0
+ * @lastmodified 2024-10-08
+ * @package Tigress\Manipulator
+ */
+class Manipulator
+{
+    public static function version(): string
+    {
+        return '1.0.0';
+    }
+}
