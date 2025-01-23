eda4342fa69a044deb8a3d5f386d5adfd4b6e7b6
RudyMasGoNext <rudy.mas@go-next.be>
2025-01-23 13:39:28 +0100

    Added Class NationalIdentification -> format the numbers according the country

--- src/Manipulator.php
+++ src/Manipulator.php
@@ -3,12 +3,12 @@
 namespace Tigress;
 
 /**
- * Class Text Manipulator (PHP version 8.4)
+ * Class Manipulator (PHP version 8.4)
  *
  * @author Rudy Mas <rudy.mas@rudymas.be>
- * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
+ * @copyright 2024-2025, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2024.12.18.0
+ * @version 2025.01.23.0
  * @package Tigress\Manipulator
  */
 class Manipulator
@@ -21,11 +21,12 @@ class Manipulator
     public static function version(): array
     {
         return [
-            'Manipulator' => '2024.12.18',
+            'Manipulator' => '2025.01.23',
             'BBCode' => BBCode::version(),
             'CalculateBirthday' => CalculateBirthday::version(),
             'CalculatePregnancy' => CalculatePregnancy::version(),
             'DateManipulator' => DateManipulator::version(),
+            'NationalIdentification' => NationalIdentification::version(),
             'TextManipulator' => TextManipulator::version(),
         ];
     }
---
+++ src/NationalIdentification.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tigress;
+
+/**
+ * Class NationalIdentification (PHP version 8.4)
+ *
+ * @author Rudy Mas <rudy.mas@rudymas.be>
+ * @copyright 2025, rudymas.be. (http://www.rudymas.be/)
+ * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * @version 2025.01.23.0
+ * @package Tigress\Manipulator
+ */
+class NationalIdentification
+{
+    public static function version(): array
+    {
+        return [
+            'NationalIdentification' => '2025.01.23',
+        ];
+    }
+
+    /**
+     * Format the national identification number by country
+     *
+     * Supported countries:
+     * - Belgium (BE)
+     * - Netherlands (NL)
+     *
+     * @param string $country
+     * @param mixed $unformattedNumber
+     * @return string
+     */
+    public function formatNumber(string $country, mixed $unformattedNumber): string
+    {
+        return match ($country) {
+            'BE' => $this->formatBelgium($unformattedNumber),
+            'NL' => $this->formatNetherlands($unformattedNumber),
+            default => $unformattedNumber,
+        };
+    }
+
+    /**
+     * Format the Belgian national identification number
+     *
+     * @param mixed $unformattedNumber
+     * @return false|string
+     */
+    private function formatBelgium(mixed $unformattedNumber): false|string
+    {
+        $unformattedNumber = preg_replace("/[^0-9]/", "", $unformattedNumber);
+        if (mb_strlen($unformattedNumber) === 11) {
+            $step1 = substr_replace($unformattedNumber, '.', 2, 0);
+            $step2 = substr_replace($step1, '.', 5, 0);
+            $step3 = substr_replace($step2, '-', 8, 0);
+            return substr_replace($step3, '.', -2, 0);
+        }
+        return false;
+    }
+
+    /**
+     * Format the Dutch national identification number
+     *
+     * @param mixed $unformattedNumber
+     * @return false|string
+     */
+    private function formatNetherlands(mixed $unformattedNumber): false|string
+    {
+        // Implement the logic to format the Dutch national identification number
+        $unformattedNumber = preg_replace("/[^0-9]/", "", $unformattedNumber);
+        if (mb_strlen($unformattedNumber) === 9) {
+            $step1 = substr_replace($unformattedNumber, '.', 4, 0);
+            return substr_replace($step1, '.', -2, 0);
+        }
+        return false;
+    }
+}
