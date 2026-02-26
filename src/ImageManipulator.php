83f38c04566a21205c46eeb3556b2ae8baf2c8ba
RudyMasGoNext <rudy.mas@go-next.be>
2026-02-26 12:07:09 +0100

    Added extra functions to ImageManipulator

--- src/ImageManipulator.php
+++ src/ImageManipulator.php
@@ -10,7 +10,7 @@ use Exception;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2026, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2026.02.26.0
+ * @version 2026.02.26.1
  * @package Tigress\TextManipulator
  */
 class ImageManipulator
@@ -37,6 +37,27 @@ class ImageManipulator
         $base64Image = base64_encode($imageData);
         $mimeType = mime_content_type($imagePath);
         return "data:$mimeType;base64,$base64Image";
+    }
+
+    /**
+     * Converts a base64 string back to an image and saves it to a temporary file.
+     * Note: The caller is responsible for deleting the temporary file after use.
+     *
+     * @param string $base64String
+     * @return string
+     * @throws Exception
+     */
+    public function getImageFromBase64(string $base64String): string
+    {
+        if (preg_match('/^data:(.*);base64,(.*)$/', $base64String, $matches)) {
+            $mimeType = $matches[1];
+            $base64Data = $matches[2];
+            $imageData = base64_decode($base64Data);
+            $tempFilePath = tempnam(sys_get_temp_dir(), 'img_' . uniqid() . '.' . $this->getExtensionFromMimeType($mimeType));
+            file_put_contents($tempFilePath, $imageData);
+            return $tempFilePath;
+        }
+        throw new Exception('Invalid base64 image string.');
     }
 
     /**
@@ -92,5 +113,22 @@ class ImageManipulator
         $base64Image = base64_encode($resizedImageData);
 
         return "data:$mimeType;base64,$base64Image";
+    }
+
+    /**
+     * Get the file extension based on the MIME type.
+     *
+     * @param string $mimeType
+     * @return string
+     * @throws Exception
+     */
+    private function getExtensionFromMimeType(string $mimeType): string
+    {
+        return match ($mimeType) {
+            'image/jpeg' => 'jpg',
+            'image/png' => 'png',
+            'image/gif' => 'gif',
+            default => throw new Exception('Unsupported MIME type: ' . $mimeType),
+        };
     }
 }
